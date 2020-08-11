bf8a249df7454bb59c9e51b0cee794de2ff17ec5
joniaj <jjonia45@gmail.com>
2020-08-11 12:00:31 +0300

    add changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,63 +1,52 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.png';
-import './nlabapp.css'
-
-class Header extends Component {
+import logo from './github/logo.png';
+class header extends Component {
   render() {
     return (
 <div className="header">
-  <div className="logo">
-    <img src={logo}/>
-    <h1>NLAB INNOVATION ACADEMY</h1>
-    </div>
+  <div className="logo"><img/></div>
+<div className="title"><h1>NLAB INNOVATION ACADEMY</h1></div>
 <div className="nav">
   <a href="Index.js">HOME</a>
-  <a href="aboutnlab.js">ABOUT NLAB</a>
+  <a href="about.js">ABOUT NLAB</a>
   <a href="service.js">OUR SERVICES</a>
   <a href="contact.js">CONTACT US</a>
 </div>
 </div>
 );
 }
-}
+  }
   const Body=()=>{
     return(
-      <div>
       <div className="row">
-        <div className="colum">
-        <h1>OUR PROGRAMS</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="colum">
-         <h3> Diploma In IT</h3>
+        <div colum>
+          <h1>OUR PROGRAMS</h1>
+         <p> Diploma In IT</p>
           <p>We are providing software technology 
             and ICT skills.</p>
         </div>
-        <div className="colum">
-         <h3> Certificate In IT</h3>
+        <div colum>
+         <p> Certificate In IT</p>
           <p>We are providing technical and technological skills required
             by</p>
         </div>
-        <div className="colum">
-         <h3> Short course</h3>
+        <div colum>
+         <p> Short course</p>
           <p>We are welcoming even student who have not passed well
             in their studies they are starting with VETA after that 
             they will be allowed to continue with certificates.
           </p>
         </div>
       </div>
-
-      <div className="row cta">
+      <div class="row cta">
     <h2>What can we help you?</h2>
     <button>Apply now</button>
-     </div>
-
+</div>
 <div className="row">
 <div className="colum">
- <h2> Do you what to join Nlab?</h2>
-  <h3>As student</h3>
+  Do you what to join Nlab?
+  <p>As student</p>
   <p> Nlab is helping a student like you to make sure
     your good educated in technology field.
   </p>
@@ -73,30 +62,25 @@ class Header extends Component {
       </p>
      </div>
 </div>
-</div>
     )
   }
-
-  function Footer() {
+  function footer() {
     return(
       <div className="footer">
         <p>info@Nlab.ac.tz</p>
       </div>
     )
   }
-
   class App extends Component{
     render(){
       return(
-        <div>
-          <Header/>
-          <Body/>
-          <Footer/>
-        </div>
+      <header/>
+      <Body/>
+      <footer/>
     )
   }
   }
-  ReactDOM.render(<App/>,document.getElementById("root"));
+  ReactDOM.render(<app/>,document.getElementById("root"));
 
 
 
