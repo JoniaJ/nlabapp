baefbe836ee705f212960d3ea084c98090ce9ec1
joniaj <jjonia45@gmail.com>
2020-08-13 14:53:45 +0300

    Updates

---
+++ precache-manifest.72a7aa5ac098ab112d265ecde1079c86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca356fc10e0e8d9d74451f0efc9b2a9",
+    "url": "/nlabapp/index.html"
+  },
+  {
+    "revision": "c6782aee1ceddc3697a5",
+    "url": "/nlabapp/static/css/main.a478b6f5.chunk.css"
+  },
+  {
+    "revision": "e01024a28469c32025c0",
+    "url": "/nlabapp/static/js/2.de7894b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nlabapp/static/js/2.de7894b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6782aee1ceddc3697a5",
+    "url": "/nlabapp/static/js/main.9737d010.chunk.js"
+  },
+  {
+    "revision": "e01c29363979fec0a1ed",
+    "url": "/nlabapp/static/js/runtime-main.85f25e7c.js"
+  },
+  {
+    "revision": "9038c41973bf2ee6b4fe540f8313ebc6",
+    "url": "/nlabapp/static/media/img1.9038c419.jpg"
+  },
+  {
+    "revision": "c72abd624552eabe80cd757faf038493",
+    "url": "/nlabapp/static/media/img2.c72abd62.jpg"
+  },
+  {
+    "revision": "c70b6f76cec09d969bf1e374444350a1",
+    "url": "/nlabapp/static/media/img3.c70b6f76.jpg"
+  },
+  {
+    "revision": "6f76424eee9fc3eb7db9b274be484f0c",
+    "url": "/nlabapp/static/media/logo.6f76424e.png"
+  }
+]);
